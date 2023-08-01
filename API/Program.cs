2175ebe7679e1331ac57bd6c4f87b63e8b90cfa0
Abdelrahman Ali <[abdo.ali1012@outlook.com]>
2023-08-01 18:38:11 +0300

    EndOfSection 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,7 +8,9 @@ builder.Services.AddDbContext<DataContext>(opt=>
 {
 opt.UseSqlite(builder.Configuration.GetConnectionString("DefualtConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
 // Configure the HTTP request pipeline.
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 app.Run();
